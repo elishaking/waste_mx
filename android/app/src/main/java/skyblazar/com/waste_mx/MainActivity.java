7b16dd1d137818de3f91687fad14eda3056b8129
King Elisha <ek.chibueze@gmail.com>
2019-03-20 08:54:55 +0100

    project init

---
+++ android/app/src/main/java/skyblazar/com/waste_mx/MainActivity.java
@@ -0,0 +1,13 @@
+package skyblazar.com.waste_mx;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
