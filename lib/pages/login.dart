d709bffa76b58349416e7930516065ca0a3616f7
King Elisha <ek.chibueze@gmail.com>
2019-03-24 19:53:13 +0100

    added all active elements to login page

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -10,48 +10,94 @@ class LoginPage extends StatelessWidget{
         title: Text('Login'),
       ),
       body: Container(
-        child: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.email),
-                      labelText: 'email'
-                    ),
-                    validator: (String value) {
-                      if(value.isEmpty){
-                        return 'Your email is required';
-                      } else if(!RegExp(r'^[a-z]+@[a-z]+\.[a-z]+$').hasMatch(value.toLowerCase())){
-                        return 'Please enter a valid email';
-                      }
-                    },
-                  ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Icons.lock),
-                        labelText: 'password'
-                    ),
-                    validator: (String value) {
-                      if(value.isEmpty){
-                        return 'Your email is required';
-                      } else if(!RegExp(r'^[a-z]+@[a-z]+\.[a-z]+$').hasMatch(value.toLowerCase())){
-                        return 'Please enter a valid email';
-                      }
-                    },
-                  ),
-                  RaisedButton(
-                    child: Text('Login'),
-                    onPressed: (){
+        alignment: Alignment.center,
+        padding: EdgeInsets.symmetric(horizontal: 18.0, vertical: 0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text('Sign In With'),
+            ButtonBar(
+              alignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(Icons.face),
+                  onPressed: () {
 
-                    },
-                  )
-                ],
-              ),
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.face),
+                  onPressed: () {
+
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.face),
+                  onPressed: () {
+
+                  },
+                ),
+              ],
             ),
-          ),
+            Form(
+              key: _formKey,
+              child: SingleChildScrollView(
+                child: Container(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.email),
+                            labelText: 'email'
+                        ),
+                        validator: (String value) {
+                          if(value.isEmpty){
+                            return 'Your email is required';
+                          } else if(!RegExp(r'^[a-z]+@[a-z]+\.[a-z]+$').hasMatch(value.toLowerCase())){
+                            return 'Please enter a valid email';
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            labelText: 'password'
+                        ),
+                        validator: (String value) {
+                          if(value.isEmpty){
+                            return 'Your email is required';
+                          } else if(!RegExp(r'^[a-z]+@[a-z]+\.[a-z]+$').hasMatch(value.toLowerCase())){
+                            return 'Please enter a valid email';
+                          }
+                        },
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        child: FlatButton(
+                          child: Text('Forgot Password'),
+                          onPressed: () {
+
+                          },
+                        ),
+                      ),
+                      RaisedButton(
+                        child: Text('Login'),
+                        onPressed: (){
+
+                        },
+                      ),
+                      FlatButton(
+                        child: Text("Don't have an account, Sign Up"),
+                        onPressed: () {
+
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
