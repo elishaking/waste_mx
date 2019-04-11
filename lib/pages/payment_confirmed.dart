a4ad0e6640046cb7782e52f5dcbad4eb3957c67a
King Elisha <ek.chibueze@gmail.com>
2019-04-12 00:38:23 +0100

    payment confirmed

--- lib/pages/payment_confirmed.dart
+++ lib/pages/payment_confirmed.dart
@@ -9,28 +9,48 @@ class PaymentConfirmedPage extends StatelessWidget{
 
   PaymentConfirmedPage(this.amount);
 
+  double _targetWidth = 0;
+
+  double _getSize(final double default_1440){
+    return (default_1440 / 14) * (0.0027 * _targetWidth + 10.136);
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) {   
+    _targetWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Payment Confirmed"),
       ),
       body: Center(
         child: Container(
-          padding: EdgeInsets.symmetric(vertical: 18, horizontal: 10),
+          padding: EdgeInsets.symmetric(vertical: 18, horizontal: _getSize(18)),
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              Text('Wallet Balance'),
-              customText.HeadlineText(text: 'N 5,000', textColor: Theme.of(context).primaryColor,),
-              SizedBox(height: 25,),
-              customText.BodyText(
-                text: '${amount.toString()} NGN has been debited from your wallet. Your money is safe in',
-                textColor: Colors.black,
-                textAlign: TextAlign.center,
-                textOverflow: TextOverflow.clip,
+              Image(
+                image: AssetImage('assets/wallet.png'),
+                height: _getSize(100),
               ),
-              SizedBox(height: 25,),
+              SizedBox(height: _getSize(25),),
+              Container(
+                padding: EdgeInsets.all(_getSize(10)),
+                decoration: BoxDecoration(
+                  border: Border.all(width: 1, color: Theme.of(context).primaryColor),
+                  borderRadius: BorderRadius.circular(100)
+                ),
+                child: customText.BodyText(
+                  text: '${amount.toString()} NGN has been debited from your wallet. Your money is safe in',
+                  textColor: Theme.of(context).primaryColor,
+                  textAlign: TextAlign.center,
+                  textOverflow: TextOverflow.clip,
+                ),
+              ),
+              SizedBox(height: _getSize(25),),
+              Text('Wallet Balance'),
+              SizedBox(height: _getSize(15),),
+              customText.HeadlineText(text: 'N 5,000', textColor: Theme.of(context).primaryColor,),
+              SizedBox(height: _getSize(25),),
               RaisedButton(
                 child: customText.BodyText(text: 'Track Payments', textColor: Colors.white),
                 onPressed: (){
