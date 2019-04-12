a80f5991da4c8c55ed7fcf6ab51e3a23a8bae738
King Elisha <ek.chibueze@gmail.com>
2019-04-12 17:35:01 +0100

    server code for storing images

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const functions = require('firebase-functions');
+const cors = require('cors')({
+  origin: true
+})
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+exports.storeImage = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if(req.method !== 'POST'){
+      return res.status(500).json({
+        message: 'Not allowed'
+      });
+    }
+
+    if(!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')){
+      return res.status(403).json({
+        error: 'Unauthorized'
+      })
+    }
+
+    let idToken;
+    idToken = req.headers.authorization.split('Bearer ')[1];
+
+    
+  });
+});
