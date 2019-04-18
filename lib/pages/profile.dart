91935c80005c4fc020aad19c504f105b4a9a9db1
King Elisha <ek.chibueze@gmail.com>
2019-04-18 07:43:51 +0100

    scopemodel desc profile

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,9 +1,121 @@
 import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+
+import '../scoped_models/main.dart';
+import '../models/user.dart';
 
 import './profile_edit.dart';
 import './profile_pic_view.dart';
 
 class ProfilePage extends StatelessWidget {
+  Container _buildProfile(BuildContext context, MainModel model) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 36, horizontal: 18),
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(bottom: 18),
+            child: GestureDetector(
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => ProfilePicViewPage()
+                ));
+              },
+              child: Hero(
+                tag: 'profile_pic',
+                child: CircleAvatar(
+                  backgroundImage: AssetImage('assets/profile.png'),
+                  radius: 70,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 18),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Bilikiss Adebiyi, Abiola',
+                  style: TextStyle(
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                Container(
+                  padding:
+                      EdgeInsets.symmetric(vertical: 3, horizontal: 9),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 7,
+                  ),
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Text(
+                    'Verified',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                Text('Last Login: 1min'),
+                /*Row(
+                children: <Widget>[
+                  Text('250,000')
+                ],
+              )*/
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 18),
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  title: Text('email'),
+                  subtitle: Text('bilikiss@gmail.com'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('phone number'),
+                  subtitle: Text('08037849123'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('account type'),
+                  subtitle: Text('Individual'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('location'),
+                  subtitle: Text('Allen Avenue, Ikeja Lagos'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('Simple Ads'),
+                  subtitle: Text('Unlimited'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('Featured Ads'),
+                  subtitle: Text('Unlimited'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('Expiration Date'),
+                  subtitle: Text('12/03/2020 2.30PM'),
+                ),
+                Divider(),
+                ListTile(
+                  title: Text('3.493.939'),
+                  subtitle: Text('Unlimited'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final double deviceWidth = MediaQuery.of(context).size.width;
@@ -30,110 +142,10 @@ class ProfilePage extends StatelessWidget {
       body: Container(
         width: deviceWidth,
         child: SingleChildScrollView(
-          child: Container(
-            margin: EdgeInsets.symmetric(vertical: 36, horizontal: 18),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.only(bottom: 18),
-                  child: GestureDetector(
-                    onTap: (){
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (BuildContext context) => ProfilePicViewPage()
-                      ));
-                    },
-                    child: Hero(
-                      tag: 'profile_pic',
-                      child: CircleAvatar(
-                        backgroundImage: AssetImage('assets/profile.png'),
-                        radius: 70,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 18),
-                  child: Column(
-                    children: <Widget>[
-                      Text(
-                        'Bilikiss Adebiyi, Abiola',
-                        style: TextStyle(
-                          fontFamily: 'Montserrat',
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Container(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 3, horizontal: 9),
-                        margin: EdgeInsets.symmetric(
-                          vertical: 7,
-                        ),
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
-                            borderRadius: BorderRadius.circular(10)),
-                        child: Text(
-                          'Verified',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                      ),
-                      Text('Last Login: 1min'),
-                      /*Row(
-                      children: <Widget>[
-                        Text('250,000')
-                      ],
-                    )*/
-                    ],
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(top: 18),
-                  child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        title: Text('email'),
-                        subtitle: Text('bilikiss@gmail.com'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('phone number'),
-                        subtitle: Text('08037849123'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('account type'),
-                        subtitle: Text('Individual'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('location'),
-                        subtitle: Text('Allen Avenue, Ikeja Lagos'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('Simple Ads'),
-                        subtitle: Text('Unlimited'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('Featured Ads'),
-                        subtitle: Text('Unlimited'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('Expiration Date'),
-                        subtitle: Text('12/03/2020 2.30PM'),
-                      ),
-                      Divider(),
-                      ListTile(
-                        title: Text('3.493.939'),
-                        subtitle: Text('Unlimited'),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+          child: ScopedModelDescendant<MainModel>(
+            builder: (BuildContext context, Widget child, MainModel model){
+              return _buildProfile(context, model);
+            },
           ),
         ),
       ),
