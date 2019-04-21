6d607c842bdee05686e3eb289d6b060039aa8db1
King Elisha <ek.chibueze@gmail.com>
2019-04-21 09:04:08 +0100

    search page

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search'),
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor
+        ),
+        elevation: 0,
+        title: TextField(
+          
+          style: TextStyle(
+            fontSize: 20
+          ),
+          // decoration: InputDecoration(
+            
+          //   labelText: 'Search Vendors',
+          // ),
+        )
       ),
       body: Center(
         child: Text('Search Vendors - TODO'),
