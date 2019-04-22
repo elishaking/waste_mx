602d365d06bbcde36942b8511155c3ae9026edaa
King Elisha <ek.chibueze@gmail.com>
2019-04-22 12:52:37 +0100

    view transactions

---
+++ lib/pages/view_transaction.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ViewTransactionPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+    );
+  }
+}
