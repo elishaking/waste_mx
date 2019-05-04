e9c21f48117dcba901fa715c1f4fce492fc51a27
King Elisha <ek.chibueze@gmail.com>
2019-05-04 03:46:26 +0100

    dispose waste adj

--- lib/pages/dispose_waste.dart
+++ lib/pages/dispose_waste.dart
@@ -7,6 +7,8 @@ import '../models/offering.dart';
 import '../widgets/bottom_nav.dart';
 import '../widgets/custom_text.dart' as customText;
 
+import '../utils/responsive.dart';
+
 import './select_offer_type.dart';
 
 class DisposeWastePage extends StatelessWidget {
@@ -38,12 +40,6 @@ class DisposeWastePage extends StatelessWidget {
     },
   ];
   */
-
-  double _targetWidth = 0;
-
-  double _getSize(final double default_1440) {
-    return (default_1440 / 14) * (0.0027 * _targetWidth + 10.136);
-  }
 
 /*
   List<Widget> _buildCategories(BuildContext context){
@@ -89,27 +85,28 @@ class DisposeWastePage extends StatelessWidget {
           borderRadius: BorderRadius.circular(5),
           side: BorderSide(color: Theme.of(context).primaryColor)),
       color: Colors.white,
-      padding: EdgeInsets.symmetric(
-          horizontal: _getSize(18), vertical: _getSize(18)),
-      child: SizedBox(
-        width: _getSize(120),
-        height: _getSize(120),
-        child: Column(
-          children: <Widget>[
-            Image(
-              width: _getSize(70),
-              height: _getSize(70),
-              image: AssetImage(imageUrl),
-            ),
-            SizedBox(
-              height: _getSize(15),
-            ),
-            customText.BodyText(
+      padding: EdgeInsets.symmetric(horizontal: getSize(context, 30), vertical: getSize(context, 30)),
+      child: Column(
+        children: <Widget>[
+          Image(
+            width: getSize(context, 90),
+            height: getSize(context, 90),
+            image: AssetImage(imageUrl),
+          ),
+          SizedBox(
+            height: getSize(context, 15),
+          ),
+          Container(
+            width: getSize(context, 140),
+            alignment: Alignment.center,
+            child: customText.BodyText(
               text: wasteType,
               textColor: Theme.of(context).primaryColor,
-            )
-          ],
-        ),
+              fontSize: 17,
+              fontWeight: FontWeight.w700,
+            ),
+          )
+        ],
       ),
       onPressed: () {
         Navigator.of(context).push(MaterialPageRoute(
@@ -120,8 +117,7 @@ class DisposeWastePage extends StatelessWidget {
 
   Widget _buildCategoriesSection(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(
-          horizontal: _getSize(20), vertical: _getSize(30)),
+      padding: EdgeInsets.symmetric(vertical: getSize(context, 30)),
       child: _buildAllCategories(context),
     );
   }
@@ -139,7 +135,7 @@ class DisposeWastePage extends StatelessWidget {
           ],
         ),
         SizedBox(
-          height: _getSize(15),
+          height: getSize(context, 30),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -151,7 +147,7 @@ class DisposeWastePage extends StatelessWidget {
           ],
         ),
         SizedBox(
-          height: _getSize(15),
+          height: getSize(context, 30),
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -168,8 +164,6 @@ class DisposeWastePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    _targetWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Dispose Waste'),
@@ -178,18 +172,18 @@ class DisposeWastePage extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-            height: _getSize(200),
+            height: getSize(context, 200),
             color: Theme.of(context).primaryColor,
           ),
           SingleChildScrollView(
             child: Container(
-              padding: EdgeInsets.all(_getSize(18)),
+              padding: EdgeInsets.symmetric(vertical: getSize(context, 18)),
               child: Column(
                 children: <Widget>[
                   Padding(
-                    padding: EdgeInsets.symmetric(vertical: _getSize(20)),
+                    padding: EdgeInsets.symmetric(vertical: getSize(context, 20)),
                     child: Image(
-                      height: _getSize(50),
+                      height: getSize(context, 50),
                       image: AssetImage('assets/garbage-can.png'),
                     ),
                   ),
